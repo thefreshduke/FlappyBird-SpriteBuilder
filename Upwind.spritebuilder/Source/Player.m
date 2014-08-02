2e82cd3b91b5a0f18d839c1f42e631c879341d32
thefreshduke <scottyshaw1@gmail.com>
2014-08-02 16:36:41 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ Upwind.spritebuilder/Source/Player.m
@@ -0,0 +1,13 @@
+//
+//  Player.m
+//  Upwind
+//
+//  Created by Scotty Shaw on 8/1/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+@end
